b9bd29cb7adc27c5c6940339132ae8725f5784b4
Bill Mårtensson <bill@magictechnology.se>
2021-11-18 09:07:30 +0100

    Initial Commit

---
+++ piaxfbswiftui/piaxfbswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  piaxfbswiftuiApp.swift
+//  piaxfbswiftui
+//
+//  Created by Bill Martensson on 2021-11-18.
+//
+
+import SwiftUI
+
+@main
+struct piaxfbswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
